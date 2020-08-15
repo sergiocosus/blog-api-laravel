997b704410a57fc227e48e96fabaaf0c34df1d38
Sergio Contreras Sustaita <sergiocosus@gmail.com>
2020-08-15 15:54:06 -0500

    Added categories to links and use common implementation with posts

---
+++ database/migrations/2020_08_15_194836_create_categorizables_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCategorizablesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('categorizables', function (Blueprint $table) {
+            $table->unsignedBigInteger('category_id');
+            $table->morphs('categorizable');
+            $table->foreign('category_id')->references('id')->on('categories');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('categorizables');
+    }
+}
